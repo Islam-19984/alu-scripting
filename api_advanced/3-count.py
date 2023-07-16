b4e640dd903ebbc517e3cac5e333b4b6534a31ba
Islam-19984 <i.hemada@alustudent.com>
2023-07-16 21:06:14 +0000

    3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-""" 3-count.py """
+"""Module for count_words function"""
+
 import json
 import requests
 
 
-def count_words(subreddit, word_list, after="", count=[]):
-    """ prints a sorted count of given keywords """
-
-    if after == "":
-        count = [0] * len(word_list)
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    request = requests.get(url,
-                           params={'after': after},
+def count_words(subreddit, word_list, after='', hot_list=[]):
+    """Function that queries the Reddit API."""
+    if after == '':
+        hot_list = [0] * len(word_list)
+    url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    request = requests.get(url, params={'after': after},
                            allow_redirects=False,
-                           headers={'User-Agent': 'Mozilla/5.0'})
-
+                           headers={'User-Agent': 'My User Agent 1.0'})
     if request.status_code == 200:
         data = request.json()
 
@@ -23,31 +21,31 @@ def count_words(subreddit, word_list, after="", count=[]):
             for word in topic['data']['title'].split():
                 for i in range(len(word_list)):
                     if word_list[i].lower() == word.lower():
-                        count[i] += 1
+                        hot_list[i] += 1
 
         after = data['data']['after']
         if after is None:
             save = []
             for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        count[i] += count[j]
+                for k in range(i + 1, len(word_list)):
+                    if word_list[i].lower() == word_list[k].lower():
+                        save.append(k)
+                        hot_list[i] += hot_list[k]
 
             for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (count[j] > count[i] or
-                            (word_list[i] > word_list[j] and
-                             count[j] == count[i])):
-                        aux = count[i]
-                        count[i] = count[j]
-                        count[j] = aux
-                        aux = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = aux
+                for k in range(i, len(word_list)):
+                    if (hot_list[k] > hot_list[i] or
+                            (word_list[i] > word_list[k] and
+                             hot_list[k] == hot_list[i])):
+                        a = hot_list[i]
+                        hot_list[i] = hot_list[k]
+                        hot_list[k] = a
+                        a = word_list[i]
+                        word_list[i] = word_list[k]
+                        word_list[k] = a
 
             for i in range(len(word_list)):
-                if (count[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), count[i]))
+                if (hot_list[i] > 0) and i not in save:
+                    print("{}: {}".format(word_list[i].lower(), hot_list[i]))
         else:
-            count_words(subreddit, word_list, after, count)
+            count_words(subreddit, word_list, after, hot_list)
